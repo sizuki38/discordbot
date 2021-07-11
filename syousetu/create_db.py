f5cadfb98b1af93c7647c9ab396fb3457decfa76
sizuki38 <c0117032f2@edu.teu.ac.jp>
2021-07-11 23:37:13 +0900

    add create_db.py

---
+++ syousetu/create_db.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+import sqlalchemy
+import sqlalchemy.orm
+import sqlalchemy.sql
+import sqlalchemy.ext.declarative
+
+
+engine = sqlalchemy.create_engine('sqlite:///ncode.db', echo = True)
+Base = sqlalchemy.ext.declarative.declarative_base()
+
+#----------------------
+class Novel(Base):
+	__tablename__ = 'novels'
+	id = sqlalchemy.Column(sqlalchemy.Integer, primary_key = True)
+	ncode = sqlalchemy.Column(sqlalchemy.String,unique = True)
+	nos = sqlalchemy.Column(sqlalchemy.Integer)
+	channel_ids = sqlalchemy.Column(sqlalchemy.String)
+
+#----------------------
+Session = sqlalchemy.orm.sessionmaker(bind = engine)
+session = Session()
+
+Base.metadata.create_all(engine)
