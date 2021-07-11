ff76629a4d71fb448ba07f6163bd868b6a955a15
sizuki38 <c0117032f2@edu.teu.ac.jp>
2021-07-12 07:25:28 +0900

    loop

--- syosetu/main.py
+++ syosetu/main.py
@@ -1,10 +1,10 @@
+from create_db import *
 from sub import *
 import discord, datetime, asyncio
 
 TOKEN = ''
 CHANNEL_ID = [863471180945817640,863471268070162458,863148987522482188]
 client = discord.Client()
-channel_name = 'なろう更新'
 
 @client.event
 async def greet(dt_now):
@@ -18,7 +18,16 @@ async def on_ready():#起動時
     while True:
         dt_now = datetime.datetime.now()
         if dt_now.minute%5 == 0:
-            await greet(dt_now)
+            novels = ReadNovel()
+            for novel in novels:
+                nos_now = scrapy(novel.ncode)
+                if nos_now > novel.nos:
+                    UpdateNovel(novel, nos_now)
+                    middles = ReadMiddleNovel(novel.id)
+                    for middle in middles:
+                        channel_id = ReadChannelID(middle.channel_id)
+                        channel = client.get_channel(channel_id)
+                        channel.send('小説が更新されました.\n'+base+novel.ncode+novel.nos)
         await asyncio.sleep(60)
 
 @client.event
@@ -26,17 +35,14 @@ async def on_message(message):
     if message.author.bot:
         return
     if client.user in message.mentions:
-#         try:
-        new_channel = await create_channel(message)
-            # チャンネルのリンクと作成メッセージを送信
-        text = f'{new_channel.mention} を作成しました.'
-        await message.channel.send(text)
-        await new_channel.send('aaa\nbbb')
-
+        await create_channel(message)
     if message.content.startswith('/add'):
-        url = message.content.split()[1]
-        await crapy(url)
-
+        try:
+            url = message.split()[1].split('/')[-1]
+        except:
+            await help()
+        else:
+            await scrapy(url)
 
     if message.content.startswith('/neko'):
         await message.channel.send('にゃーん')
